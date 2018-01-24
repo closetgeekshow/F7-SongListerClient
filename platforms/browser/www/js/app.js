7fdd522cb6f158d6a8ce5656b4b65575d5229e11
closetgeekshow <closetgeekshow@gmail.com>
2018-01-24 00:39:51 -0500

    login fixed

---
+++ platforms/browser/www/js/app.js
@@ -0,0 +1,38 @@
+// Dom7
+var $$ = Dom7;
+
+// Theme
+var theme = 'ios';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'com.songsmithkaraoke.songlister-client',
+  root: '#myApp',
+  theme: theme,
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  routes: routes,
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+  console.log("Device is ready!");
+});
+
---
+++ www/kitchen-sink/js/app.js
@@ -0,0 +1,54 @@
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  root: '#app',
+  theme: theme,
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  routes: routes,
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});
+
+// Option 2. Using live 'page:init' event handlers for each page
+$(document).on('page:init', '.page[data-name="login"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log('login page loaded');
+  $('#login-form').on('form:storedata', function (e) {
+      console.log('data stored');
+      var formData = app.form.convertToData('#login-form');
+      console.log('data: ' + JSON.stringify(formData));
+  }); 
+})
+
+$(document).on('page:init', '.page[data-name="add"]', function (e) {
+  // Do something here when page with data-name="about" attribute loaded and initialized
+  console.log('add page loaded');
+  $('.convert-form-to-data').on('click', function(){
+	  
+        var formData = app.form.convertToData('#add-song-form');
+        alert(JSON.stringify(formData));
+		
+    });
+})
